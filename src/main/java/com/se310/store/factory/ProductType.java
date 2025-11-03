12d2e4a71e49d908de60bc264cfd20e23d83d3ab
avadecrist <decristofaro@chapman.edu>
2025-11-02 21:07:20 -0800

    adding zach's code

---
+++ src/main/java/com/se310/store/factory/ProductType.java
@@ -0,0 +1,14 @@
+package main.java.com.se310.store.factory;
+
+
+
+// Created this class, it's a public enum to handle different product types for Product Factory
+// Since it wasn't defined originally, this seemed best to meet specifications
+// without modifying the model classes where it wasn't needed
+
+
+public enum ProductType {
+    STANDARD,
+    DISCOUNTED,
+    PREMIUM
+}
